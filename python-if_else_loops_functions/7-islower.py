5a5881a7bc54725a7e3bb56dd771e610f1e821c0
JoacoZeba <joacozeba18@gmail.com>
2025-09-08 02:12:55 +0000

    .

---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+def islower(c):
+    return 96 < ord(c) < 123
---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    str_upper = ""
+    for char in str:
+        str_upper += chr(ord(char) - 32) if 96 < ord(char) < 123 else char
+
+    print("{:s}".format(str_upper))
