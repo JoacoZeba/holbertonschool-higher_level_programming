2bf91a0da2703e1da5996d11babe3222416773b7
JoacoZeba <joacozeba18@gmail.com>
2025-09-29 02:24:47 +0000

    .

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+
+class VerboseList(list):
+
+    def append(self, item):
+        super().append(item)
+        print(f"Added {item} to the list.")
+
+    def extend(self, iterable):
+        length = len(self)
+        super().extend(iterable)
+        nitems_add = len(self) - length
+        print(f"Extended the list with {nitems_add} items.")
+
+    def remove(self, item):
+        print(f"Removed {item} from the list.")
+        super().remove(item)
+
+    def pop(self, index=-1):
+        try:
+            item = self[index]
+        except IndexError:
+            raise
+        print(f"Popped {item} from the list.")
+        return super().pop(index)
