305d54aa6f2e1e4af4387f2bbf5a3208a600e8c8
JoacoZeba <joacozeba18@gmail.com>
2025-12-17 11:37:50 +0000

    .

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const argv = require('process').argv;
+
+const av = argv.slice(2);
+
+if (av.length < 1) {
+  console.log('No argument');
+}
+
+if (av.length === 1) {
+  console.log('Argument found');
+}
+
+if (av.length > 1) {
+  console.log('Arguments found');
+}
---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const argv = require('process').argv;
+const av = argv.slice(2);
+
+console.log(add(parseInt(av[0], 10), parseInt(av[1], 10)));
+
+function add (a, b) {
+  return a + b;
+}
